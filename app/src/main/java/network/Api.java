ab17fc8dc5fc2a37b830cf1c4dc6eaa9dc454be7
Rosina Mothibi <rmmadilekere198@gmail.com>
2022-04-08 15:08:57 +0200

    First commit of this project

---
+++ app/src/main/java/network/Api.java
@@ -0,0 +1,15 @@
+package network;
+
+import java.util.List;
+
+import model.Hero;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface Api {
+
+    String BASE_URL = "https://simplifiedcoding.net/demos/";
+
+    @GET("marvel")
+    Call<List<Hero>> getHeroes();
+}
